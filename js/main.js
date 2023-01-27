923ff3b35566c7164a14333b18444d37af620feb
Abdulxamid04 <sayfullayevabdulxamid@gmail.com>
2023-01-27 21:11:02 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+   let user = documnet.querySelector('#user')
+   let phone = documnet.querySelector('#phone')
+   let adress = document.querySelector('#adress')
+   let Pizzaorder = documnet.querySelector('Pizza')
+   let xajm1 = doxumnet.querySelector('xajm1')
+   let xajm2 = documnet.querySelector('.xajm2')
+   let xajm3 = document.querySelector('.xajm3')
+   let Zakaz = documet.querySelector('#btnOrder')
+
+
+console.log('#user')
